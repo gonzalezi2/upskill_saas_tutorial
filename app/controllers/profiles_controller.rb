e40861a38a3915b45e48d7191719f20698f18a79
Ivan Gonzalez <gonzalezim02@gmail.com>
2017-01-12 19:11:51 +0000

    Added barebones user show page and updated profiles redirect

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,21 +7,20 @@ class ProfilesController < ApplicationController
   end
   
   # POST to /users/:user_id/profile
-  def create
-    # Ensure that we have the user who is filling out the form
-    @user = User.find( params[:user_id] )
-    # Create profile linked to this specific user
-    @profile = @user.build_profile(profile_params)
-    if @profile.save
-      flash[:success] = "Profile is updated!"
-      redirect_to root_path
-    else
-      render action: :new
-    end
+def create
+  # Ensure that we have the user who is filling out form
+  @user = User.find( params[:user_id] )
+  # Create profile linked to this specific user
+  @profile = @user.build_profile( profile_params )
+  if @profile.save
+    flash[:success] = "Profile updated!"
+    redirect_to user_path( params[:user_id])
+  else
+    render action: :new
   end
-  
-  private
-    def profile_params
-      params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
-    end
+end
+private
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description)
+  end
 end
