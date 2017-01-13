b653e6e0f1a75cc518844e578a038595935ecd5e
Ivan Gonzalez <gonzalezim02@gmail.com>
2017-01-13 17:00:25 +0000

    Added profile edit action and view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,20 +7,27 @@ class ProfilesController < ApplicationController
   end
   
   # POST to /users/:user_id/profile
-def create
-  # Ensure that we have the user who is filling out form
-  @user = User.find( params[:user_id] )
-  # Create profile linked to this specific user
-  @profile = @user.build_profile( profile_params )
-  if @profile.save
-    flash[:success] = "Profile updated!"
-    redirect_to user_path( params[:user_id])
-  else
-    render action: :new
+  def create
+    # Ensure that we have the user who is filling out form
+    @user = User.find( params[:user_id] )
+    # Create profile linked to this specific user
+    @profile = @user.build_profile( profile_params )
+    if @profile.save
+      flash[:success] = "Profile updated!"
+      redirect_to user_path( params[:user_id])
+    else
+      render action: :new
+    end
   end
-end
-private
-  def profile_params
-    params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
+  
+  # GET to /users/:user_id/profile/edit
+  def edit
+    @user = User.find( params[:user_id] )
+    @profile = @user.profile
   end
+    
+  private
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
+    end
 end
