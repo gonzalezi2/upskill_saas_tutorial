933677de9ab3653ca1954f5e7cb945bf9f1bda27
Ivan Gonzalez <gonzalezim02@gmail.com>
2017-01-13 17:55:12 +0000

    Added before filters to check correct user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  # Block profiles pages from non-logged in users
+  before_action :authenticate_user!
+  
   # GET to /users/:id
   def show
     @user = User.find( params[:id] )
