c833dd49347e5716ebe7005105d030e1696277ae
Ivan Gonzalez <gonzalezim02@gmail.com>
2017-01-13 18:41:30 +0000

    Added users index view and action details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   # Block profiles pages from non-logged in users
   before_action :authenticate_user!
   
+  def index
+    @users = User.includes(:profile)
+  end
+  
   # GET to /users/:id
   def show
     @user = User.find( params[:id] )
